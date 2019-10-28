e02c3cfc5c3f347e3b8c52282726ea741abf1d80
PlsDeleteMyLife <cdranton@gmail.com>
2019-10-29 00:03:04 +0700

    Start for kids

---
+++ AVS/3/cmake-build-debug/CMakeFiles/AVS3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AVS3"
+  "AVS3.pdb"
+  "CMakeFiles/AVS3.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AVS3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
