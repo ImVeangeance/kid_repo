f6f1011cfac1f5c7f50804a30edbe4b413c68891
PlsDeleteMyLife <cdranton@gmail.com>
2019-12-02 19:34:30 +0700

    Fx

--- AVS/5/main.cpp
+++ AVS/5/main.cpp
@@ -2,12 +2,12 @@
 #include <chrono>
 #include <iomanip>
 #include <pthread.h>
+#include <ctime>
 #include <vector>
-#include <string>
 #include <random>
 
-int_fast64_t $size = 0, pthread_size, thread_id;
-std::vector <int_fast64_t > x, result;
+int64_t $size = 0, pthread_size, thread_id;
+std::vector <int64_t > x, result;
 std::vector <std::vector <double_t>> matrix;
 
 void *num_gen(void *arg);
@@ -23,6 +23,7 @@ int main()
     x.resize($size, 0);
     matrix.resize($size, std::vector <double_t>($size));
     threads = new pthread_t[pthread_size];
+    auto t1 = std::chrono::high_resolution_clock::now();
     for(auto i = 0; i < pthread_size; ++i)
     {
         thread_id = i;
@@ -32,30 +33,45 @@ int main()
     {
         pthread_join(pthread_size, nullptr);
     }
+    auto t2 = std::chrono::high_resolution_clock::now();
     for(auto i = 0; i < pthread_size; ++i)
     {
         thread_id = i;
         pthread_create(&threads[i], nullptr, multiply, &thread_id);
     }
+    auto t3 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration <double_t> elapsedSeconds{};
+    std::cout << "\nRandom & computing time: " << std::endl;
+    elapsedSeconds = t3 - t1;
+    std::cout << elapsedSeconds.count() << " seconds." << std::endl;
+    std::cout << std::chrono::duration_cast <std::chrono::nanoseconds>(t3 - t1).count() << " nanoseconds." << std::endl;
+    std::cout << "\nRandom time: " << std::endl;
+    elapsedSeconds = t2 - t1;
+    std::cout << elapsedSeconds.count() << " seconds." << std::endl;
+    std::cout << std::chrono::duration_cast <std::chrono::nanoseconds>(t2 - t1).count() << " nanoseconds." << std::endl;
+    std::cout << "\nComputing time: " << std::endl;
+    elapsedSeconds = t3 - t2;
+    std::cout << elapsedSeconds.count() << " seconds." << std::endl;
+    std::cout << std::chrono::duration_cast <std::chrono::nanoseconds>(t3 - t2).count() << " nanoseconds." << std::endl;
     return 0;
 }
 
 void *num_gen(void *arg)
 {
-
-    std::default_random_engine generator;
-    std::uniform_int_distribution <int_fast64_t> distribution(1, 500);
-    int_fast64_t threadID, chunk, alpha, omega;
-    threadID = *(int_fast64_t *)arg;
+    std::mt19937(time(nullptr));
+    std::default_random_engine ss;
+    std::uniform_int_distribution <int64_t> distribution(1, 1000);
+    int64_t threadID, chunk, alpha, omega;
+    threadID = *(int64_t *)arg;
     chunk = $size / pthread_size;
     alpha = threadID * chunk;
     omega = (threadID + 1) * chunk;
     for(auto i = alpha; i < omega; ++i)
     {
-        x[i] = distribution(generator);
+        x[i] = distribution(ss);
         for(auto j = 0; j < $size; ++j)
         {
-            matrix[i][j] = distribution(generator);
+            matrix[i][j] = distribution(ss);
         }
     }
     pthread_exit(nullptr);
@@ -63,8 +79,8 @@ void *num_gen(void *arg)
 
 void *multiply(void *arg)
 {
-    int_fast64_t threadID, chunk, alpha, omega;
-    threadID = *(int_fast64_t *)arg;
+    int64_t threadID, chunk, alpha, omega;
+    threadID = *(int64_t *)arg;
     chunk = $size / pthread_size;
     alpha = threadID * chunk;
     omega = (threadID + 1) * chunk;
